28b870eeebd28e1201e3c0ac065ca9911692c8c6
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2023-07-20 09:23:08 +0200

    fix: sonarlint
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/test/java/org/gridsuite/shortcircuit/server/TestUtils.java
+++ src/test/java/org/gridsuite/shortcircuit/server/TestUtils.java
@@ -4,14 +4,16 @@ import org.springframework.cloud.stream.binder.test.OutputDestination;
 
 import java.util.List;
 
-import static org.junit.Assert.assertNull;
+public final class TestUtils {
+    //private static final int TIMEOUT = 100;
 
-public class TestUtils {
-    private static final int TIMEOUT = 100;
+    private TestUtils() {
+
+    }
 
     public static void assertQueuesEmptyThenClear(List<String> destinations, OutputDestination output) {
         try {
-            //TODO: almost all tests are breaking if I uncomment this. To fix in another PR
+            //TODO: almost all tests are breaking if we uncomment this. To fix in another PR
             //destinations.forEach(destination -> assertNull("Should not be any messages in queue " + destination + " : ", output.receive(TIMEOUT, destination)));
         } catch (NullPointerException e) {
             // Ignoring
