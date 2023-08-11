d6443f4057918d1eb856d4e7391ab6f9002c2a56
Hugo Marcellin <hugo.marcelin@rte-france.com>
2023-08-11 14:22:57 +0200

    Fix code smell

--- src/main/java/org/gridsuite/shortcircuit/server/entities/FeederResultEmbeddable.java
+++ src/main/java/org/gridsuite/shortcircuit/server/entities/FeederResultEmbeddable.java
@@ -28,19 +28,17 @@ public class FeederResultEmbeddable {
     private double current;
 
     @Embedded
-    @AttributeOverrides({
-        @AttributeOverride(name = "positiveMagnitude", column = @Column(name = "fortescue_current_positive_magnitude")),
-        @AttributeOverride(name = "zeroMagnitude", column = @Column(name = "fortescue_current_zero_magnitude")),
-        @AttributeOverride(name = "negativeMagnitude", column = @Column(name = "fortescue_current_negative_magnitude")),
-        @AttributeOverride(name = "positiveAngle", column = @Column(name = "fortescue_current_positive_angle")),
-        @AttributeOverride(name = "zeroAngle", column = @Column(name = "fortescue_current_zero_angle")),
-        @AttributeOverride(name = "negativeAngle", column = @Column(name = "fortescue_current_negative_angle")),
-        @AttributeOverride(name = "magnitudeA", column = @Column(name = "fortescue_current_magnitude_a")),
-        @AttributeOverride(name = "magnitudeB", column = @Column(name = "fortescue_current_magnitude_b")),
-        @AttributeOverride(name = "magnitudeC", column = @Column(name = "fortescue_current_magnitude_c")),
-        @AttributeOverride(name = "angleA", column = @Column(name = "fortescue_current_angle_a")),
-        @AttributeOverride(name = "angleB", column = @Column(name = "fortescue_current_angle_b")),
-        @AttributeOverride(name = "angleC", column = @Column(name = "fortescue_current_angle_c")),
-    })
+    @AttributeOverride(name = "positiveMagnitude", column = @Column(name = "fortescue_current_positive_magnitude"))
+    @AttributeOverride(name = "zeroMagnitude", column = @Column(name = "fortescue_current_zero_magnitude"))
+    @AttributeOverride(name = "negativeMagnitude", column = @Column(name = "fortescue_current_negative_magnitude"))
+    @AttributeOverride(name = "positiveAngle", column = @Column(name = "fortescue_current_positive_angle"))
+    @AttributeOverride(name = "zeroAngle", column = @Column(name = "fortescue_current_zero_angle"))
+    @AttributeOverride(name = "negativeAngle", column = @Column(name = "fortescue_current_negative_angle"))
+    @AttributeOverride(name = "magnitudeA", column = @Column(name = "fortescue_current_magnitude_a"))
+    @AttributeOverride(name = "magnitudeB", column = @Column(name = "fortescue_current_magnitude_b"))
+    @AttributeOverride(name = "magnitudeC", column = @Column(name = "fortescue_current_magnitude_c"))
+    @AttributeOverride(name = "angleA", column = @Column(name = "fortescue_current_angle_a"))
+    @AttributeOverride(name = "angleB", column = @Column(name = "fortescue_current_angle_b"))
+    @AttributeOverride(name = "angleC", column = @Column(name = "fortescue_current_angle_c"))
     private FortescueResultEmbeddable fortescueCurrent;
 }
