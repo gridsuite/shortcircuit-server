51f9eece94b827b8eaf07148c3ba834b9f5337ff
Hugo Marcellin <hugo.marcelin@rte-france.com>
2023-08-17 15:39:13 +0200

    Add missing licence

--- src/main/java/org/gridsuite/shortcircuit/server/utils/ShortcircuitUtils.java
+++ src/main/java/org/gridsuite/shortcircuit/server/utils/ShortcircuitUtils.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.shortcircuit.server.utils;
 
 import com.powsybl.iidm.network.extensions.util.FortescueUtil;
@@ -6,6 +12,9 @@ import com.powsybl.shortcircuit.FortescueValue;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 import org.apache.commons.math3.util.Pair;
 
+/**
+ * @author Hugo Marcellin <hugo.marcelin at rte-france.com>
+ */
 public final class ShortcircuitUtils {
 
     private ShortcircuitUtils() {
