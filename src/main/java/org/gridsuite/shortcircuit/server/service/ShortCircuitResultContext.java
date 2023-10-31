eaf1343264be816cc8339a39ae1734c09d99b1e0
Abdelsalem <abdelsalem.hedhili@rte-france.com>
2023-10-31 14:38:31 +0100

    remove unused method
    Signed-off-by: Abdelsalem <abdelsalem.hedhili@rte-france.com>

--- src/main/java/org/gridsuite/shortcircuit/server/service/ShortCircuitResultContext.java
+++ src/main/java/org/gridsuite/shortcircuit/server/service/ShortCircuitResultContext.java
@@ -17,7 +17,6 @@ import org.springframework.messaging.support.MessageBuilder;
 
 import java.io.UncheckedIOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.gridsuite.shortcircuit.server.service.NotificationService.*;
 
@@ -42,16 +41,6 @@ public class ShortCircuitResultContext {
     public ShortCircuitResultContext(UUID resultUuid, ShortCircuitRunContext runContext) {
         this.resultUuid = Objects.requireNonNull(resultUuid);
         this.runContext = Objects.requireNonNull(runContext);
-    }
-
-    private static List<UUID> getHeaderList(MessageHeaders headers, String name) {
-        String header = (String) headers.get(name);
-        if (header == null || header.isEmpty()) {
-            return Collections.emptyList();
-        }
-        return Arrays.asList(header.split(",")).stream()
-            .map(UUID::fromString)
-            .collect(Collectors.toList());
     }
 
     private static String getNonNullHeader(MessageHeaders headers, String name) {
