39b8e40beaac67d7e92719ada0fe7d2b369a4fb7
etiennehomer <etiennehomer@gmail.com>
2023-12-18 12:28:15 +0100

    Remove hashCode() and equals() overrides + clean result entities annotation (#66)
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/shortcircuit/server/entities/FaultResultEntity.java
+++ src/main/java/org/gridsuite/shortcircuit/server/entities/FaultResultEntity.java
@@ -31,7 +31,7 @@ public class FaultResultEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID faultResultUuid;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @Setter
     private ShortCircuitAnalysisResultEntity result;
 
@@ -129,22 +129,6 @@ public class FaultResultEntity {
     public void setFeederResults(List<FeederResultEntity> feederResults) {
         this.feederResults = feederResults;
         feederResults.stream().forEach(feederResultEntity -> feederResultEntity.setFaultResult(this));
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof FaultResultEntity)) {
-            return false;
-        }
-        return faultResultUuid != null && faultResultUuid.equals(((FaultResultEntity) o).getFaultResultUuid());
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
     }
 
     public double getPositiveMagnitude() {
