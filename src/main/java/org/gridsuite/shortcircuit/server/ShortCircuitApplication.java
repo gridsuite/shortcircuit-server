e1a4532ad7d077ea1794104dea46f5491f8b41a4
Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>
2024-05-28 16:56:39 +0200

    Fix SpringBoot scan package (#93)
    Signed-off-by: Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>

--- src/main/java/org/gridsuite/shortcircuit/server/ShortCircuitApplication.java
+++ src/main/java/org/gridsuite/shortcircuit/server/ShortCircuitApplication.java
@@ -9,16 +9,13 @@ package org.gridsuite.shortcircuit.server;
 import com.powsybl.network.store.client.NetworkStoreService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Etienne Homer <etienne.homer at rte-france.com>
  */
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
-@SpringBootApplication
-@ComponentScan(basePackageClasses = {ShortCircuitApplication.class, NetworkStoreService.class})
+@SpringBootApplication(scanBasePackageClasses = { ShortCircuitApplication.class, NetworkStoreService.class })
 public class ShortCircuitApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ShortCircuitApplication.class, args);
     }
