8e0e1000862df93993a36a352092b474cb0d16ce
TOURI ANIS <anis-1.touri@rte-france.com>
2024-05-31 13:36:41 +0200

    remove comment
    Signed-off-by: TOURI ANIS <anis-1.touri@rte-france.com>

--- src/test/java/org/gridsuite/shortcircuit/server/ShortCircuitAnalysisControllerTest.java
+++ src/test/java/org/gridsuite/shortcircuit/server/ShortCircuitAnalysisControllerTest.java
@@ -545,7 +545,6 @@ public class ShortCircuitAnalysisControllerTest {
 
     @Test
     public void runWithBusIdTest() throws Exception {
-        //We need to limit the precision to avoid database precision storage limit issue (postgres has a precision of 6 digits while h2 can go to 9)
         try (MockedStatic<ShortCircuitAnalysis> shortCircuitAnalysisMockedStatic = Mockito.mockStatic(ShortCircuitAnalysis.class)) {
             shortCircuitAnalysisMockedStatic.when(() -> ShortCircuitAnalysis.runAsync(eq(network), anyList(), any(ShortCircuitParameters.class), any(ComputationManager.class), anyList(), any(ReportNode.class)))
                 .thenReturn(CompletableFuture.completedFuture(ShortCircuitAnalysisResultMock.RESULT_FORTESCUE_FULL));
@@ -607,7 +606,6 @@ public class ShortCircuitAnalysisControllerTest {
 
     @Test
     public void runWithBusBarSectionIdTest() throws Exception {
-        //We need to limit the precision to avoid database precision storage limit issue (postgres has a precision of 6 digits while h2 can go to 9)
         try (MockedStatic<ShortCircuitAnalysis> shortCircuitAnalysisMockedStatic = Mockito.mockStatic(ShortCircuitAnalysis.class)) {
             shortCircuitAnalysisMockedStatic.when(() -> ShortCircuitAnalysis.runAsync(eq(nodeBreakerNetwork), anyList(), any(ShortCircuitParameters.class), any(ComputationManager.class), anyList(), any(ReportNode.class)))
                 .thenReturn(CompletableFuture.completedFuture(ShortCircuitAnalysisResultMock.RESULT_FORTESCUE_FULL));
@@ -650,7 +648,6 @@ public class ShortCircuitAnalysisControllerTest {
 
     @Test
     public void runWithBusBarSectionIdAndErrorTest() throws Exception {
-        //We need to limit the precision to avoid database precision storage limit issue (postgres has a precision of 6 digits while h2 can go to 9)
         try (MockedStatic<ShortCircuitAnalysis> shortCircuitAnalysisMockedStatic = Mockito.mockStatic(ShortCircuitAnalysis.class)) {
             shortCircuitAnalysisMockedStatic.when(() -> ShortCircuitAnalysis.runAsync(eq(nodeBreakerNetwork), anyList(), any(ShortCircuitParameters.class), any(ComputationManager.class), anyList(), any(ReportNode.class)))
                 .thenReturn(CompletableFuture.completedFuture(ShortCircuitAnalysisResultMock.RESULT_MAGNITUDE_FULL));
