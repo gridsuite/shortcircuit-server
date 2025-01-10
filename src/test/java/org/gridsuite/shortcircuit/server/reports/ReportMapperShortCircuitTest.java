0c482a5343dc9e26983f7cbb915c75375a1ab8f7
Franck LECUYER <franck.lecuyer@rte-france.com>
2025-01-10 12:49:35 +0100

    Replace temporarly null provider with "default-provider" (waiting for fix in powsybl-ws-commons handling null provider) Remove override of powsybl-ws-commons version
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/test/java/org/gridsuite/shortcircuit/server/reports/ReportMapperShortCircuitTest.java
+++ src/test/java/org/gridsuite/shortcircuit/server/reports/ReportMapperShortCircuitTest.java
@@ -33,7 +33,7 @@ class ReportMapperShortCircuitTest extends AbstractReportMapperTest {
     @Test
     void testAggregatedLogs() throws Exception {
         ShortCircuitRunContext runContext = new ShortCircuitRunContext(null, "variantId", "receiver", new ShortCircuitParameters(),
-            null, "reporterId", "reportType", "userId", "provider", "busId");
+            null, "reporterId", "reportType", "userId", "default-provider", "busId");
         runContext.setVoltageLevelsWithWrongIsc(Collections.singletonList("VL1"));
         final ReportNode result = reportMapper.processReporter(rootReportNode, runContext);
         log.debug("Result = {}", RestTemplateConfig.objectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(result));
