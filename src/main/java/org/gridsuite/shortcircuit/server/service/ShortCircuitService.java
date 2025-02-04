f834a2bd1ee8acf70e37074705b8ac78e6373ef7
Antoine Bouhours <antoine.bouhours@rte-france.com>
2025-02-04 10:25:18 +0100

    Fix IEC909 (#124) (#126)
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/shortcircuit/server/service/ShortCircuitService.java
+++ src/main/java/org/gridsuite/shortcircuit/server/service/ShortCircuitService.java
@@ -53,10 +53,12 @@ public class ShortCircuitService extends AbstractComputationService<ShortCircuit
 
     // This voltage intervals' definition is not clean and we could potentially lose some buses.
     // To be cleaned when VoltageRange uses intervals that are open on the right.
+    // TODO: to be moved to RTE private config or to powsybl-rte-core
     public static final List<VoltageRange> CEI909_VOLTAGE_PROFILE = List.of(
             new VoltageRange(0, 199.999, 1.1),
-            new VoltageRange(200.0, 299.999, 1, 245),
-            new VoltageRange(300.0, 450.0, 1, 420)
+            new VoltageRange(200.0, 299.999, 1.09),
+            new VoltageRange(300.0, 389.99, 1.10526),
+            new VoltageRange(390.0, 410.0, 1.05)
     );
 
     private final ParametersRepository parametersRepository;
