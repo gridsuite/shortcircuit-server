8e6fd84954e2f9c89b88fa03598e405f9ee89a69
Igor PIROG <igor.pirog@rte-france.com>
2025-08-21 13:43:12 +0200

    Adjust test on FilterService
    Signed-off-by: Igor PIROG <igor.pirog@rte-france.com>

--- src/test/java/org/gridsuite/shortcircuit/server/service/FilterServiceTest.java
+++ src/test/java/org/gridsuite/shortcircuit/server/service/FilterServiceTest.java
@@ -6,37 +6,58 @@
  */
 package org.gridsuite.shortcircuit.server.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.powsybl.iidm.network.Country;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.VariantManager;
 import com.powsybl.network.store.client.NetworkStoreService;
 import com.powsybl.network.store.client.PreloadingStrategy;
+import mockwebserver3.Dispatcher;
+import mockwebserver3.MockResponse;
+import mockwebserver3.MockWebServer;
+import mockwebserver3.RecordedRequest;
+import mockwebserver3.junit5.internal.MockWebServerExtension;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
 import org.gridsuite.computation.dto.GlobalFilter;
 import org.gridsuite.computation.dto.ResourceFilterDTO;
 import org.gridsuite.filter.AbstractFilter;
+import org.gridsuite.filter.expertfilter.ExpertFilter;
+import org.gridsuite.filter.expertfilter.expertrule.AbstractExpertRule;
+import org.gridsuite.filter.expertfilter.expertrule.NumberExpertRule;
+import org.gridsuite.filter.utils.EquipmentType;
+import org.gridsuite.filter.utils.expertfilter.FieldType;
+import org.gridsuite.filter.utils.expertfilter.OperatorType;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import java.io.IOException;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockWebServerExtension.class)
 @SpringBootTest
 class FilterServiceTest {
 
     private static final String NETWORK_UUID = "7928181c-7977-4592-ba19-88027e4254e4";
-
     private static final String VARIANT_ID = "variant_id";
-
     private static final UUID LIST_UUID = UUID.randomUUID();
+    private static final Object TEST_FILTERS = List.of(createTestExpertFilter());
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Mock
     private Network network;
@@ -49,6 +70,42 @@ class FilterServiceTest {
 
     @Autowired
     private FilterService filterService;
+
+    private static ExpertFilter createTestExpertFilter() {
+        AbstractExpertRule simpleRule = NumberExpertRule.builder()
+                .value(220.0)
+                .field(FieldType.NOMINAL_VOLTAGE)
+                .operator(OperatorType.EQUALS)
+                .build();
+        return new ExpertFilter(FilterServiceTest.LIST_UUID, new Date(), EquipmentType.VOLTAGE_LEVEL, simpleRule);
+    }
+
+    @BeforeEach
+    void setUp(final MockWebServer mockWebServer) throws Exception {
+        filterService = new FilterService(networkStoreService, initMockWebServer(mockWebServer));
+        doNothing().when(variantManager).setWorkingVariant(anyString());
+    }
+
+    private String initMockWebServer(final MockWebServer server) throws IOException {
+        String jsonFiltersExpected = objectMapper.writeValueAsString(TEST_FILTERS);
+        final Dispatcher dispatcher = new Dispatcher() {
+            @NotNull
+            @Override
+            public MockResponse dispatch(RecordedRequest request) {
+                String requestPath = Objects.requireNonNull(request.getPath());
+                if (requestPath.matches("/v1/filters/metadata\\?ids=.*")) {
+                    return new MockResponse(HttpStatus.OK.value(), Headers.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE), jsonFiltersExpected);
+                } else {
+                    return new MockResponse.Builder().code(HttpStatus.NOT_FOUND.value()).body("Path not supported: " + request.getPath()).build();
+                }
+            }
+        };
+        server.setDispatcher(dispatcher);
+
+        // Ask the server for its URL. You'll need this to make HTTP requests.
+        HttpUrl baseHttpUrl = server.url("");
+        return baseHttpUrl.toString().substring(0, baseHttpUrl.toString().length() - 1);
+    }
 
     @Test
     void testGetResourceFiltersWithAllFilters() {
@@ -123,8 +180,7 @@ class FilterServiceTest {
         List<AbstractFilter> result = filterService.getFilters(List.of());
         assertTrue(result.isEmpty());
 
-        List<UUID> filterUuids = List.of(LIST_UUID);
-        List<AbstractFilter> filters = filterService.getFilters(filterUuids);
+        List<AbstractFilter> filters = filterService.getFilters(List.of(LIST_UUID));
         assertNotNull(filters);
     }
 }
