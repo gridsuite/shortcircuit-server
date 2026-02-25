5416b9c834843ba5061ec0a3d0187326bafa70bc
Etienne LESOT <etienne.lesot@rte-france.com>
2026-02-25 11:17:35 +0100

    fix
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/main/resources/db/changelog/changesets/powerElectronicsParametersMigration.sql
+++
@@ -1,6 +0,0 @@
-UPDATE short_circuit_specific_parameters
-SET value_ = REPLACE(value_, '"type":"WIND"', '"type":"GENERATOR"')
-where name = 'powerElectronicsClusters';
-UPDATE short_circuit_specific_parameters
-SET value_ = REPLACE(value_, '"type":"SOLAR"', '"type":"GENERATOR"')
-where name = 'powerElectronicsClusters';
